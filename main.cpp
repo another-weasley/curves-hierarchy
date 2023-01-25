0b8ca333c79eab93401750645bf08f9d45fff8b5
Anna Kozlovskaya <annhelkoz@gmail.com>
2023-01-25 14:20:46 +0300

    - populate a container of random curves with random parameters - add create_random_object function - change standart (from 11 to 14)

--- main.cpp
+++ main.cpp
@@ -4,20 +4,46 @@
 #include <math.h>
 #include "ellipse.hpp"
 #include "helix.hpp"
+#include <vector>
+#include <memory>
+#include <stdlib.h>
 
-using namespace std;
-
+std::shared_ptr <ICurve> create_random_object(int k)
+{
+    std::shared_ptr<ICurve> ptr = nullptr;
+    // число четное и делится на 5 или 3 - создаем спираль
+    if (k%2 == 0 && (k%5 == 0 || k%3 == 0))
+    {
+        ptr = std::make_shared<Helix>(std::rand(), std::rand());
+    }
+    // число четное, но не делится на 3 или 5 - создаем эллипс
+    else if (k%2 == 0)
+    {
+        ptr = std::make_shared<Ellipse>(std::rand(), std::rand());
+    }
+    // число нечетное - создаем круг
+    else
+    {
+        ptr = std::make_shared<Circle>(std::rand());
+    }
+    return ptr;
+}
 int main()
 {
-    cout << "Hello World!" << endl;
-    Point3D p(3, 0, 2);
-    cout << "point: " << p << endl;
-    Circle circ(1);
+    // populate vector random manner with random parameters
+    std::vector <std::shared_ptr<ICurve>> curve_cont;
+    int n = 10;
+    for (int i = 0; i < n; ++i)
+    {
+        curve_cont.push_back(create_random_object(i + std::rand()));
+    }
+    // print coordinates of points and derivatives of all curves in the container at t=PI/4
     float t = 3.14/4;
-    cout << circ.calc_at(t) << " " << circ.calc_derivative_at(t) << endl;
-    Ellipse elp(2, 1);
-    cout << elp.calc_at(t) << " " << elp.calc_derivative_at(t) <<  endl;
-    Helix hell(1, 1);
-    cout << hell.calc_at(t) << " " << hell.calc_derivative_at(t) << endl;
+    for (int i = 0; i < n; ++i)
+    {
+        std::cout << "Point: " << curve_cont[i]->calc_at(t) << " ";
+        std::cout << "First derivative: " << curve_cont[i]->calc_derivative_at(t) << std::endl;
+    }
+
     return 0;
 }
